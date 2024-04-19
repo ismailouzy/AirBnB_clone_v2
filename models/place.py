d52446b874c65f28366ed4ec820eee6e4cdacb60
ismailouzy <ismailouzal@gmail.com>
2024-04-19 16:21:13 -0700

    d

--- models/place.py
+++ models/place.py
@@ -5,39 +5,20 @@ from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from models.city import City
 from models.user import User
+from models.amenity import Amenity
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False)
+                      )
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        place_amenity = Table(
-            'place_amenity', Base.metadata,
-            Column('place_id', String(60), ForeignKey('places.id'),
-                   primary_key=True, nullable=False),
-            Column('amenity_id', String(60), ForeignKey('amenities.id'),
-                   primary_key=True, nullable=False)
-        )
-        amenities = relationship("Amenity", secondary=place_amenity)
-    else:
-        @property
-        def amenities(self):
-            """ Getter attribute for amenities in FileStorage """
-            from models import storage
-            from models.amenity import Amenity
-
-            amenity_list = []
-            for amenity in storage.all(Amenity).values():
-                if amenity.id in self.amenity_ids:
-                    amenity_list.append(amenity)
-            return amenity_list
-
-        @amenities.setter
-        def amenities(self, obj=None):
-            """ Setter attribute for amenities in FileStorage """
-            if isinstance(obj, Amenity):
-                self.amenity_ids.append(obj.id)
 
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
@@ -67,3 +48,18 @@ class Place(BaseModel, Base):
                 list_reviews.append(review)
 
         return review
+
+    @property
+    def amenities(self):
+        """ Getter attribute for amenities in FileStorage """
+        amenity_list = []
+        for amenity in storage.all(Amenity).values():
+            if amenity.id in self.amenity_ids:
+                amenity_list.append(amenity)
+        return amenity_list
+
+    @amenities.setter
+    def amenities(self, obj=None):
+        """ Setter attribute for amenities in FileStorage """
+        if isinstance(obj, Amenity):
+            self.amenity_ids.append(obj.id)
