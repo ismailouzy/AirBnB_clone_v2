3e0a05205fc51160bc6cc0be18feed9e2b0a3dc2
ismailouzy <ismailouzal@gmail.com>
2024-05-03 15:11:01 -0700

    d

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#script that sets up your web servers
 
-# Install Nginx if it's not already installed
+# Install Nginx and allow HTTP traffic
 sudo apt-get update
-sudo apt-get install -y nginx
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
+# Create necessary directories
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
 
-sudo echo "Hello, World!" > /data/web_static/releases/test/index.html
+# Create test index.html file
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 
-sudo rm -f /etc/nginx/sites-enabled/web_static
-sudo ln -s /data/web_static/releases/test /data/web_static/current
+# Create symbolic link
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
+# Set ownership to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data
 
-sudo echo "server {
-    listen 80;
-    server_name _;
-    location /hbnb_static {
-        alias /data/web_static/current;
-    }
-}" > /etc/nginx/sites-available/web_static
+# Update Nginx configuration
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-sudo ln -s /etc/nginx/sites-available/web_static /etc/nginx/sites-enabled/
-
+# Restart Nginx
 sudo service nginx restart
