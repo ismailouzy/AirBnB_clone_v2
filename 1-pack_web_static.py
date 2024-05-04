40e6f02f3f30e856f8c0e52c2bb4c97a31ab91f4
ismailouzy <ismailouzal@gmail.com>
2024-05-03 17:48:04 -0700

    d

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Compress before sending"""
+
+
+from fabric.api import *
+from datetime import datetime
+
+
+def do_pack():
+    """
+    makin
+    """
+
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
+        return archive
+    else:
+        return None
