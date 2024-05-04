b5c801e34eda4c92564a6153f34706cacca3b6d2
ismailouzy <ismailouzal@gmail.com>
2024-05-03 18:01:53 -0700

    d

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
+
+from fabric.api import put, run, env
+from os.path import exists, basename, splitext
+
+env.hosts = ['107.21.39.78', '100.24.255.89']
+
+
+def do_deploy(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
+        return False
+
+    try:
+        filename = basename(archive_path)
+        no_ext, _ = splitext(filename)
+        path = "/data/web_static/releases/"
+
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(
+            filename, path, no_ext))
+        run('rm /tmp/{}'.format(filename))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(
+            path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(
+            path, no_ext))
+
+        return True
+    except FileNotFoundError:
+        return False
