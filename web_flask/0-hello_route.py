41bb3de8e354572a26e84180e873d6576f55fed5
ismailouzy <ismailouzal@gmail.com>
2024-05-18 14:55:03 +0000

    d

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Airbnb clone flask application"""
+
+from flask import Flask, abort, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """index page of the Airbnb clone"""
+    return 'Hello HBNB'
+
+app.run(host='0.0.0.0' , port=5000)
