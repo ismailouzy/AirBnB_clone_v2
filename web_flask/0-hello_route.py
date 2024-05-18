17b2b2cdd9b8a27a36f28ec0887a68d31e2f782d
ismailouzy <ismailouzal@gmail.com>
2024-05-18 08:26:53 -0700

    d

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def index():
     """index page of the Airbnb clone"""
-    return 'Hello HBNB'
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
